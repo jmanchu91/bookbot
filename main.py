360a432e7f1d29d3b38cec99004b4660e4c02498
jmanchu91 <jtmccoy91@gmail.com>
2025-03-10 13:42:04 -0400

    Created character sorting function in descending order

--- main.py
+++ main.py
@@ -5,12 +5,22 @@ def get_book_text(filepath):
 
 from stats import get_num_words
 
+from stats import character_counter
+
+from stats import sort_character_count
+
 def main():
     num_words = 0
+    char_count = {}
     book_text = get_book_text("books/frankenstein.txt")
     num_words = get_num_words(book_text)
-
+    char_count = character_counter(book_text)
+    sorted_count = sort_character_count(char_count)
+    print("============ BOOKBOT ============"
+    "Analyzing book found at books/frankestein.txt")
     print(num_words, "words found in the document")
-
+    print(char_count)
+    print(sorted_count)
+    
 main()
 
--- stats.py
+++ stats.py
@@ -10,3 +10,16 @@ def character_counter(file_contents):
         else:
             letter_dictionary[char] = 1
     return letter_dictionary    
+
+
+def sort_on(dict):
+    return dict["count"]
+
+def sort_character_count(char_count):
+    sorted_characters = [
+        {"character": ch, "count": count}
+        for ch, count in char_count.items()
+        if ch.isalpha()
+    ]
+    sorted_characters.sort(reverse=True, key=sort_on)
+    return sorted_characters
