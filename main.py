68ab2c901d8511936f590ee45c288e744679c169
jmanchu91 <jtmccoy91@gmail.com>
2025-03-10 14:11:09 -0400

    Finalized the bookbot

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import sys
 
+if len(sys.argv) < 2:
+    print("Usage: python2 main.py <path_to_book>")
+    sys.exit(1)
+path = sys.argv[1]
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
@@ -14,7 +19,7 @@ from stats import sort_character_count
 def main():
     num_words = 0
     char_count = {}
-    book_text = get_book_text("books/frankenstein.txt")
+    book_text = get_book_text(path)
     num_words = get_num_words(book_text)
     char_count = character_counter(book_text)
     sorted_count = sort_character_count(char_count)
